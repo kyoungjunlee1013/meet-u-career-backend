744ca72cd33555a61130e20447de4876d54e76ec
Yottawatts <antoniomoon7879@gmail.com>
2025-04-06 23:29:04 +0900

    WIP: CAR-55 잘못된 브랜치; 기업회원: 인재 검색: 구직자 찾기 (2025-04-06)

--- src/main/java/com/highfive/meetu/domain/application/common/repository/ApplicationRepository.java
+++ src/main/java/com/highfive/meetu/domain/application/common/repository/ApplicationRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ApplicationRepository extends JpaRepository<Application, Long> {
+public interface ApplicationRepository extends JpaRepository<Application, Long>, ApplicationRepositoryCustom {
 
 }
