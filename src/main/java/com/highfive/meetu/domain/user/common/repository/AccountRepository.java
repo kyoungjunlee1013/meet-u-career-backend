f1be650415a9a3ed2f567320408897302c846c5b
000flag <vili1265@naver.com>
2025-04-06 20:24:07 +0900

    WIP: CAR-23 개인회원/기업회원 회원가입 작업 중 (2025-04-06)

--- src/main/java/com/highfive/meetu/domain/user/common/repository/AccountRepository.java
+++ src/main/java/com/highfive/meetu/domain/user/common/repository/AccountRepository.java
@@ -4,7 +4,10 @@ import com.highfive.meetu.domain.user.common.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
+    // 이메일(아이디) 중복 체크
+    boolean existsByEmail(String email);
 }
