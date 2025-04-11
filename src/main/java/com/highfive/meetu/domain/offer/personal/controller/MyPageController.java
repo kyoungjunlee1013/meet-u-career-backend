d23d638350c0a4b9c35d2f2cf98be8968e8b07a6
kyoungjunlee1013 <kyoungjun7777@naver.com>
2025-04-11 16:59:08 +0900

    WIP:CAR-59 resultdata 추가 작업완료 2025-04-11

--- src/main/java/com/highfive/meetu/domain/offer/personal/controller/MyPageController.java
+++ src/main/java/com/highfive/meetu/domain/offer/personal/controller/MyPageController.java
@@ -3,6 +3,7 @@ package com.highfive.meetu.domain.offer.personal.controller;
 
 import com.highfive.meetu.domain.offer.personal.dto.MyOfferDTO;
 import com.highfive.meetu.domain.offer.personal.service.OfferService;
+import com.highfive.meetu.global.common.response.ResultData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,16 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/mypage")
+@RequestMapping("/api/personal/mypage/offers")
 @RequiredArgsConstructor
 public class MyPageController {
 
   private final OfferService offerService;
 
 
-  @GetMapping("/my-offers/{accountId}")
-  public MyOfferDTO getMyOffersForTest(@PathVariable Long accountId) {
-    return offerService.getMyOffers(accountId);
+  @GetMapping("/list/{accountId}")
+  public ResultData<MyOfferDTO> getMyOffersForTest(@PathVariable Long accountId) {
+    MyOfferDTO dto = offerService.getMyOffers(accountId);
+    return ResultData.success(1,dto);
   }
 
 }
