c310a30736575efffaf09b0ffe39634655005696
kyoungjunlee1013 <kyoungjun7777@naver.com>
2025-04-11 16:53:53 +0900

    WIP:CAR-60 mypage-resultdata 추가 작업완료 2025-04-09

--- src/main/java/com/highfive/meetu/domain/dashboard/personal/controller/MyPageController.java
+++ src/main/java/com/highfive/meetu/domain/dashboard/personal/controller/MyPageController.java
@@ -2,11 +2,12 @@ package com.highfive.meetu.domain.dashboard.personal.controller;
 
 import com.highfive.meetu.domain.dashboard.personal.dto.MyPageDTO;
 import com.highfive.meetu.domain.dashboard.personal.service.MyPageService;
+import com.highfive.meetu.global.common.response.ResultData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/mypage")
+@RequestMapping("/api/personal/mypage")
 @RequiredArgsConstructor
 public class MyPageController {
 
@@ -14,8 +15,9 @@ public class MyPageController {
 
 
   @GetMapping("/{accountId}")
-  public MyPageDTO getMyPageTest(@PathVariable Long accountId) {
-    return myPageService.getMyPageInfo(accountId);
+  public ResultData<MyPageDTO> getMyPageTest(@PathVariable Long accountId) {
+    MyPageDTO dto =myPageService.getMyPageInfo(accountId);
+    return ResultData.success(1,dto);
   }
 
 }
