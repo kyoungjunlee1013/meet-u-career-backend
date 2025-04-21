b97a7e747864e11729a4804c5a7966f06c7a7f0d
Yottawatts <antoniomoon7879@gmail.com>
2025-04-21 09:14:09 +0900

    fix: application-secret

--- src/main/java/com/highfive/meetu/domain/application/business/controller/ApplicationController.java
+++ src/main/java/com/highfive/meetu/domain/application/business/controller/ApplicationController.java
@@ -4,6 +4,7 @@ import com.highfive.meetu.domain.application.business.dto.ApplicationDetailDTO;
 import com.highfive.meetu.domain.application.business.service.ApplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/applications")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class ApplicationController {
 
   private final ApplicationService applicationService;
