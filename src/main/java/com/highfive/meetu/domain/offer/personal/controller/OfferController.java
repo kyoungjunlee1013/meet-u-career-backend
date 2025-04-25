a1f61dcfc1335cdd4898bf253d0cdbc95bce4390
kyoungjunlee1013 <kyoungjun7777@naver.com>
2025-04-25 10:40:40 +0900

    WIP:CAR-59 personal-mypage-offer 수정  작업완료 2025-04-25

--- src/main/java/com/highfive/meetu/domain/offer/personal/controller/OfferController.java
+++ src/main/java/com/highfive/meetu/domain/offer/personal/controller/OfferController.java
@@ -4,6 +4,7 @@ package com.highfive.meetu.domain.offer.personal.controller;
 import com.highfive.meetu.domain.offer.personal.dto.MyOfferDTO;
 import com.highfive.meetu.domain.offer.personal.service.OfferService;
 import com.highfive.meetu.global.common.response.ResultData;
+import com.highfive.meetu.infra.oauth.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,28 +17,32 @@ public class OfferController {
   private final OfferService offerService;
 
 
-  @GetMapping("/list/review/{accountId}")
-  public ResultData<MyOfferDTO> getMyOffersForTest(@PathVariable Long accountId) {
-    MyOfferDTO dto = offerService.getMyOffers(accountId);
+  @GetMapping("/list/review")
+  public ResultData<MyOfferDTO> getMyOffersForTest() {
+    Long AccountId = SecurityUtil.getAccountId();
+    MyOfferDTO dto = offerService.getMyOffers(AccountId);
     return ResultData.success(1, dto);
   }
 
-  @PostMapping("/{offerId}/approve")
-  public ResultData<MyOfferDTO> approveOffer(@PathVariable Long offerId) {
-    offerService.approveOffer(offerId);
+  @PostMapping("/approve")
+  public ResultData<MyOfferDTO> approveOffer() {
+    Long OfferId = SecurityUtil.getAccountId();
+    offerService.approveOffer(OfferId);
     System.out.println("✅ Approve 호출됨");
     return ResultData.success(1, null); // 응답 컨벤션에 따라 null 본문 반환
   }
 
-  @PostMapping("/{offerId}/reject")
-  public ResultData<MyOfferDTO> rejectOffer(@PathVariable Long offerId) {
-    offerService.rejectOffer(offerId);
+  @PostMapping("/reject")
+  public ResultData<MyOfferDTO> rejectOffer() {
+    Long OfferId = SecurityUtil.getAccountId();
+    offerService.rejectOffer(OfferId);
     return ResultData.success(1, null);
   }
 
-  @GetMapping("/list/all/{accountId}")
-  public ResultData<MyOfferDTO> getAllOffers(@PathVariable Long accountId) {
-    MyOfferDTO dto = offerService.getMyOffers(accountId); // status 관계없이 전부 조회
+  @GetMapping("/list/all")
+  public ResultData<MyOfferDTO> getAllOffers() {
+    Long AccountId = SecurityUtil.getAccountId();
+    MyOfferDTO dto = offerService.getMyOffers(AccountId); // status 관계없이 전부 조회
     return ResultData.success(1, dto);
   }
 }
