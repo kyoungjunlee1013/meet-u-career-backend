1ec9a63c15a1ee19257513d97fc05c962aaf2f29
kyoungjunlee1013 <kyoungjun7777@naver.com>
2025-04-25 10:14:06 +0900

    WIP:CAR-60 personal-mypage-dashboard 수정  작업완료 2025-04-25

--- src/main/java/com/highfive/meetu/domain/dashboard/personal/controller/MyPageController.java
+++ src/main/java/com/highfive/meetu/domain/dashboard/personal/controller/MyPageController.java
@@ -3,6 +3,7 @@ package com.highfive.meetu.domain.dashboard.personal.controller;
 import com.highfive.meetu.domain.dashboard.personal.dto.MyPageDTO;
 import com.highfive.meetu.domain.dashboard.personal.service.MyPageService;
 import com.highfive.meetu.global.common.response.ResultData;
+import com.highfive.meetu.infra.oauth.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +14,14 @@ public class MyPageController {
 
   private final MyPageService myPageService;
 
+
   @CrossOrigin(
       origins = "http://localhost:3000", // 실제 프론트엔드 주소로 변경 필요
       allowCredentials = "true"
   )
-  @GetMapping("/{profileId}")
-  public ResultData<MyPageDTO> getMyPage(@PathVariable Long profileId) {
+  @GetMapping()
+  public ResultData<MyPageDTO> getMyPage() {
+    Long profileId = SecurityUtil.getProfileId();
     MyPageDTO dto = myPageService.getMyPageInfo(profileId);
     return ResultData.success(1, dto);
   }
