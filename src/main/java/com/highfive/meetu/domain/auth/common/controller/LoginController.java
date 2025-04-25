20e270627eb06c449c1ebcef0130772f32b17425
dpdlcl <dpdlcl0111@gmail.com>
2025-04-26 05:42:27 +0900

    chore: Swagger 의존성 추가 및 SwaggerConfig 설정

--- src/main/java/com/highfive/meetu/domain/auth/common/controller/LoginController.java
+++ src/main/java/com/highfive/meetu/domain/auth/common/controller/LoginController.java
@@ -4,6 +4,8 @@ import com.highfive.meetu.domain.auth.personal.dto.LoginRequestDTO;
 import com.highfive.meetu.domain.auth.personal.dto.LoginResponseDTO;
 import com.highfive.meetu.domain.auth.common.service.login.LoginService;
 import com.highfive.meetu.global.common.response.ResultData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 /**
  * 로그인(Login) 전용 API 컨트롤러
  */
+@Tag(name = "00-login-controller", description = "로그인/토큰 발급 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -22,6 +25,7 @@ public class LoginController {
      * 개인 로그인 요청
      * - AccessToken, RefreshToken 발급
      */
+    @Operation(summary = "개인회원 로그인 API")
     @PostMapping("/personal/auth/login")
     public ResponseEntity<ResultData<LoginResponseDTO>> personalLogin(@RequestBody LoginRequestDTO dto) {
         return loginService.userLogin(dto);
@@ -31,6 +35,7 @@ public class LoginController {
      * 기업 로그인 요청
      * - AccessToken, RefreshToken 발급
      */
+    @Operation(summary = "기업회원 로그인 API")
     @PostMapping("/business/auth/login")
     public ResponseEntity<ResultData<LoginResponseDTO>> businessLogin(@RequestBody LoginRequestDTO dto) {
         return loginService.businessLogin(dto);
@@ -40,6 +45,7 @@ public class LoginController {
      * 관리자 로그인 요청
      * - AccessToken, RefreshToken 발급
      */
+    @Operation(summary = "관리자 로그인 API")
     @PostMapping("/admin/auth/login")
     public ResponseEntity<ResultData<LoginResponseDTO>> adminLogin(@RequestBody LoginRequestDTO dto) {
         return loginService.adminLogin(dto);
